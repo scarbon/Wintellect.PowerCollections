d9bde0d7bf48d194de828a772e3cf1512d408615
scarbon <scarbon@gmail.com>
2025-09-13 22:04:21 +0100

    New folder structure

--- src/PowerCollections/AssemblyInfo.cs
+++ src/PowerCollections/AssemblyInfo.cs
@@ -20,36 +20,6 @@ using System.Runtime.CompilerServices;
 "0024000004800000940000000602000000240000525341310004000001000100094e056116d23654c58fb4438f80555edc1ba42f47c23ac2cf50585aa5040edda143d88717cc9405293e50e04a8b7ba419a967f7cd7a9d453b23461bbb6dd5a7df6706de63494db86f7607b7b96551f7f494375296678333f4cb606a7a1e32603cd032f369533767e919d07cbc0a3684dd842949660401f623a1d39a8b395ce9")]
 #endif
 
-
-//
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-//
-[assembly: AssemblyTitle("Wintellect.PowerCollections")]
-[assembly: AssemblyDescription("The Power Collections group of collection classes.")]
-#if DEBUG
-[assembly: AssemblyConfiguration("DEBUG")]
-#else
-[assembly: AssemblyConfiguration("")]
-#endif
-[assembly: AssemblyCompany("Wintellect")]
-[assembly: AssemblyProduct("Power Collections")]
-[assembly: AssemblyCopyright("Copyright (c) 2004-2007, Wintellect")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]		
-
-//
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Revision and Build Numbers 
-// by using the '*' as shown below:
-
-[assembly: AssemblyVersion("1.1.*")]
+// Keep only non-conflicting attributes here; the SDK will generate the rest.
 [assembly: ComVisible(false)]
 
